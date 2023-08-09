f87566851352dfec37c48ff9631984214d3ae4ff
jordandidit <jordygashagaza@gmail.com>
2023-08-09 18:58:23 +0200

    homepage

---
+++ src/pages/HomePageIndexPage1.js
@@ -0,0 +1,188 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
+import "./HomePageIndexPage1.css";
+import { Link, useHref } from "react-router-dom";
+const HomePageIndexPage1 = () => {
+  return (
+    <div className="home-page-index-page-1">
+      <b className="woodempile">WoodEmpile</b>
+      <div className="hillwood-payment">Hillwood payment</div>
+      <Link className="home">Home</Link>
+      <Link className="countries">countries</Link>
+      <Link className="company">company</Link>
+      <Link className="product">product</Link>
+      <Link className="signup">Signup</Link>
+      <div className="home-page-index-page-1-child" />
+      <div className="home-page-index-page-1-item" />
+      <Button className="signup1" variant="primary">
+       Signup
+      </Button>
+      <div className="home-page-index-page-1-inner" />
+      <Button className="login" variant="primary">
+        login
+      </Button>
+      <img className="rectangle-icon" alt="" src="/anime.png" />
+      <b className="we-accept-payment-container">
+        <p className="we-accept-payment">we Accept payment</p>
+        <p className="we-accept-payment">{`to all african country `}</p>
+      </b>
+      <div className="lorem-ipsum-dolor-container">
+        <p className="we-accept-payment">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <p className="we-accept-payment">{`Pellentesque fermentum elit augue, `}</p>
+        <p className="we-accept-payment">{`at commodo enim condimentum vitae. `}</p>
+        <p className="we-accept-payment">{`Duis porta neque eu orci eleifend varius. `}</p>
+      </div>
+      <Button className="contact-us" variant="outline-primary">
+        contact us
+      </Button>
+      <div className="rectangle-div" />
+      <div className="home-page-index-page-1-child1" />
+      <div className="lorem-ipsum-dolor-container1">
+        <p className="we-accept-payment">
+          Lorem ipsum piscing elit.
+        </p>
+        <p className="we-accept-payment">Pellentesque fermentum elit augue,</p>
+      </div>
+      <b className="choose-woodempile-payment-container">
+        <p className="we-accept-payment">choose WoodEmpile</p>
+        <p className="we-accept-payment">payment</p>
+      </b>
+      <b className="trusted-copanies-worldwide">Trusted Companies worldwide</b>
+      <div className="home-page-index-page-1-child2" />
+      <div className="home-page-index-page-1-child3" />
+      <Button className="get-started-with" variant="">
+        Get started with card
+      </Button>
+      <img className="security-icon" alt="" src="/security.svg" />
+      <div className="home-page-index-page-1-child4" />
+      <img className="payment-icon" alt="" src="/payment.svg" />
+      <div className="home-page-index-page-1-child5" />
+      <img className="message-icon" alt="" src="/message.svg" />
+      <div className="hight-security">Hight security</div>
+      <div className="payment-method">Payment Method</div>
+      <div className="customer-support">Customer Support</div>
+      <div className="lorem-ipsum-dolor2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </div>
+      <div className="lorem-ipsum-dolor3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </div>
+      <div className="lorem-ipsum-dolor4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </div>
+      <b className="lets-get-start-container">
+        <p className="we-accept-payment">let’s get start in virtual way</p>
+        <p className="we-accept-payment">and virtual card</p>
+      </b>
+      <div className="smart-card">
+        <div className="glass-pannel" />
+        <div className="glass-pannel1" />
+        <div className="glass-pannel2" />
+        <div className="div">5221-456-789-123</div>
+        <div className="emery-norbert">Emery Norbert</div>
+        <img className="wifi-icon" alt="" src="/wifi.svg" />
+        <img className="wifi-icon1" alt="" src="/wifi1.svg" />
+      </div>
+      <div className="saly-44">
+        <img className="saly-44-icon" alt="" src="/saly44@2x.png" />
+      </div>
+      <img
+        className="home-page-index-page-1-child6"
+        alt=""
+        src="/mastercard.png"
+      />
+      <div className="glass-pannel3" />
+      <div className="glass-pannel4" />
+      <div className="glass-pannel5" />
+      <div className="lorem-ipsum-dolor-container2">
+        <span className="lorem-ipsum-dolor-container3">
+          <p className="we-accept-payment">Lorem ipsum dolor sit amet, conse</p>
+          <p className="we-accept-payment">{`Pellentesque fermentum elit augue, `}</p>
+          <p className="we-accept-payment">{`at commodo enim condimentum v. `}</p>
+        </span>
+      </div>
+      <div className="lorem-ipsum-dolor-container4">
+        <span className="lorem-ipsum-dolor-container3">
+          <p className="we-accept-payment">Lorem ipsum dolor sit amet, conse</p>
+          <p className="we-accept-payment">{`Pellentesque fermentum elit augue, `}</p>
+          <p className="we-accept-payment">{`at commodo enim condimentum v. `}</p>
+        </span>
+      </div>
+      <div className="lorem-ipsum-dolor-container6">
+        <span className="lorem-ipsum-dolor-container3">
+          <p className="we-accept-payment">{`Lorem ipsum dolor sit amet, consePellentesque fermentum elit augue, at commodo enim condimentum v.Pellentesque fermentum elit augue, `}</p>
+          <p className="we-accept-payment">{`at commodo enim condimentum v `}</p>
+        </span>
+      </div>
+      <div className="div1">20+</div>
+      <div className="div2">20+</div>
+      <div className="div3">100,000+</div>
+      <div className="currencies">Currencies</div>
+      <div className="countries1">Countries</div>
+      <div className="mechants">Mechants</div>
+      <img
+        className="home-page-index-page-1-child7"
+        alt=""
+        src="/expedia.png"
+      />
+      <img
+        className="home-page-index-page-1-child8"
+        alt=""
+        src="/azam.png"
+      />
+      <img
+        className="home-page-index-page-1-child9"
+        alt=""
+        src="/radisson.png"
+      />
+      <img
+        className="home-page-index-page-1-child10"
+        alt=""
+        src="/mtn.png"
+      />
+      <img
+        className="home-page-index-page-1-child11"
+        alt=""
+        src="/booking.jpg"
+      />
+      
+      <div className="home-page-index-page-1-child12" />
+      <div className="ours-service">ours service</div>
+      <div className="about-us">About us</div>
+      <div className="contact">contact</div>
+      <Link className="home1">Home</Link>
+      <Link className="how-do-we">how do we work?</Link>
+      <div className="div4">(+250) 780 15555</div>
+      <a className="woodempiregmailcom">WoodEmpire@gmail.com</a>
+      <Link className="where-you-ask">where you ask question</Link>
+      <Link className="for-more-information">for more information</Link>
+      <Link className="about-us1">About us</Link>
+      <Link className="service">service</Link>
+      <div className="in-publishing-and-container">
+        <p className="we-accept-payment">In publishing and graphic design,</p>
+        <p className="we-accept-payment">{`Lorem ipsum is a  `}</p>
+      </div>
+      <Form.Group className="wrapper">
+        <Form.Control type="text" placeholder="Enter your Email" />
+      </Form.Group>
+      
+      <div className="home-page-index-page-1-child13" />
+      <Button className="subscribe" variant="primary">
+        subscribe
+      </Button>
+      <div className="line-div" />
+      <div className="wood-2022-">Wood 2022 - 2030.Aright reserved</div>
+      <img
+        className="home-page-index-page-1-child14"
+        alt=""
+        src="/copyright.png"
+      />
+      <b className="woodempile1">WoodEmpile</b>
+      <div className="hillwood-payment1">Hillwood payment</div>
+    </div>
+  );
+};
+
+export default HomePageIndexPage1;
